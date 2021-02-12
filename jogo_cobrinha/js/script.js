082b5ad3333f588f23960ba89d15c607f35165ba
luhrocha <luanarocha17@gmail.com>
2021-02-12 15:59:00 -0300

    Aula 03 jogo da cobrinha

--- jogo_cobrinha/js/script.js
+++ jogo_cobrinha/js/script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+
 snake[0] = {
 	x: 8 * box,
 	y: 8 * box
@@ -23,8 +24,26 @@ function criarCobrinha(){
 
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+	if(event.keyCode == 37 && direction != "right")
+		direction = "left";
+	if(event.keyCode == 38 & direction != "down") 
+		direction = "up";
+	if(event.keyCode == 39 && direction != "left") 
+		direction = "right";
+	if(event.keyCode = 40 && direction != "up") 
+		direction = "down";
+}
+
 function iniciarJogo(){
 	
+	if(snake[0].x > 15 * box && direction == "right") snake[0].x = 16 * box;
+	if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+	if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+	if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+	   
 	criarBG();
 	criarCobrinha();		
 
@@ -38,12 +57,12 @@ function iniciarJogo(){
 	
 	snake.pop();
 	
-	let novaCabeca = {
+	let newHead = {
 		x: snakeX,
 		y: snakeY
 	};
 	
-	snake.unshift(novaCabeca);
+	snake.unshift (newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100);//milisegundos
